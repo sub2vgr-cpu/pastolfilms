80553581b12673719874cd2c230541185fc6932c
sub2vgr-cpu <sub2vgr@gmail.com>
2026-08-04 09:43:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-leow
+// Smooth scrolling
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        const href = this.getAttribute('href');
+
+        if (href.startsWith("#")) {
+            e.preventDefault();
+
+            const target = document.querySelector(href);
+
+            if (target) {
+                target.scrollIntoView({
+                    behavior: "smooth"
+                });
+            }
+        }
+    });
+});
+
+// Active menu
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".sidebar nav a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const top = section.offsetTop - 100;
+
+        if (scrollY >= top) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+
+    // Home
+    if (scrollY < 100) {
+        navLinks.forEach(link => link.classList.remove("active"));
+        navLinks[0].classList.add("active");
+    }
+});
